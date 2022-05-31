d1683b25ba053d604a4599bc1305c4cc589f581c
Jiwoon Lee <metr0jw@naver.com>
2022-05-31 13:27:37 +0900

    change from row-wise to column-wise

--- HW2/assign2_2019202008.m
+++ HW2/assign2_2019202008.m
@@ -1,11 +1,14 @@
+% Generate input data as A
 t = 0.01:0.01:0.2;
-freq = 11:30;
 A = zeros(20, 20);
 for freq = 11:30 
-    A(freq-10, :) = cos(2*pi*freq*t);
+    A(:, freq-10) = cos(2*pi*freq*t);
 end
 
-A_inv_builtin = inv(A);
+% Load output.mat
+load('output.mat');
+
+A_inv_builtin = inv(A); % inverse of A
 
 % Find inverse of A using PLU factorization
 A_inv = zeros(20, 20);
